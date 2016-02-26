5afe4ef96d7239ef6f35c2a74d5e6e9c65e7db3f
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2016-02-26 17:33:03 +0100

    (bin) improving the bin/db-connect.sh script, to be able to choose between connections, and to support remote hosts

--- bin/db-connect.sh
+++ bin/db-connect.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-CONN=`grep dsn config/databases.yml | grep -v '^#' | sed "s/\s*dsn:\s*'\(.*\):host=\(.*\);dbname=\(\w*\).*/\1:\2:\3/g"`
+LINE=1
+[ "$1" -gt 1 ] && LINE=$1
 
+# user
+export PGUSER=`grep username config/databases.yml | grep -v '^#' | sed "s/\s*username:[^a-Z]*\(\w*\)$/\1/g" | head -n$LINE | tail -n1`
+
+# passwd
+export PGPASSWORD=`grep password config/databases.yml | grep -v '^#' | sed "s/\s*password:[^a-Z^0-9]*\(\w*\)$/\1/g" | head -n$LINE | tail -n1`
+
+# host & db
+CONN=`grep dsn config/databases.yml | grep -v '^#' | sed "s/\s*dsn:\s*'\(.*\):host=\(.*\);dbname=\(\w*\).*/\1:\2:\3/g" | head -n$LINE | tail -n1`
 if [ "`echo $CONN | cut -d : -f 1`" = 'pgsql' ]; then
   CMD=psql
 fi
 
-HOST=''
+# host
+export PGHOST=''
 if [ ! "`echo $CONN | cut -d : -f 2`" = 'localhost' ]; then
-  HOST=`echo $CONN | cut -d : -f 2`
+  PGHOST=`echo $CONN | cut -d : -f 2`
 fi
 
-$CMD $HOST `echo $CONN | cut -d : -f 3`
+# db
+export PGDATABASE=`echo $CONN | cut -d : -f 3`
+
+echo "+------------------------------------------------+"
+echo "| Host    : $PGHOST"
+echo "| Database: $PGDATABASE"
+echo "| User    : $PGUSER"
+echo "+------------------------------------------------+"
+echo ""
+psql
