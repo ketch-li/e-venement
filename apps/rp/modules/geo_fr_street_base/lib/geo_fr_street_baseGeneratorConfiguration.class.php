0165da8b5c483c31b2ef2f539e24a007bca2216a
Baptiste SIMON <baptiste.simon@e-glop.net>
2016-03-04 12:14:47 +0100

    (rp) adding a module which allows Ajax queries on street base

---
+++ apps/rp/modules/geo_fr_street_base/lib/geo_fr_street_baseGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * geo_fr_street_base module configuration.
+ *
+ * @package    e-venement
+ * @subpackage geo_fr_street_base
+ * @author     Baptiste SIMON <baptiste.simon AT e-glop.net>
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class geo_fr_street_baseGeneratorConfiguration extends BaseGeo_fr_street_baseGeneratorConfiguration
+{
+}
