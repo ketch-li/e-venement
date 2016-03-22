fdb2a2b93b2da5aad2b1e766aa52d29ab1978534
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2016-03-22 15:12:49 +0100

    (event) correcting some calls of partials for bad references to a foreign module (location instead of resource) refs #946

--- apps/event/modules/resource/templates/showSuccess.php
+++ apps/event/modules/resource/templates/showSuccess.php
@@ -1,5 +1,5 @@
 <?php use_helper('I18N', 'Date') ?>
-<?php include_partial('location/assets') ?>
+<?php include_partial('assets') ?>
 
 <div id="sf_admin_container" class="sf_admin_show ui-widget ui-widget-content ui-corner-all">
   <div class="fg-toolbar ui-widget-header ui-corner-all">
@@ -7,7 +7,7 @@
   </div>
 
   <div class="sf_admin_actions_block ui-widget">
-      <?php include_partial('location/show_actions', array('form' => $form, 'location' => $location, 'configuration' => $configuration, 'helper' => $helper)) ?>
+      <?php include_partial('show_actions', array('form' => $form, 'location' => $location, 'configuration' => $configuration, 'helper' => $helper)) ?>
   </div>
 
   <div class="ui-helper-clearfix"></div>
@@ -16,5 +16,5 @@
 
   <?php include_partial('show_footer', array('location' => $location, 'form' => $form, 'configuration' => $configuration)) ?>
 
-  <?php include_partial('location/themeswitcher') ?>
+  <?php include_partial('themeswitcher') ?>
 </div>
