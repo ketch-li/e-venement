1e632d5ffd8d4521621e5a1304c149c747a3c2ed
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-04-15 16:25:26 +0200

    (pub) Added an option to disable 3D Secure on Paybox payments - refs #1111

--- plugins/liOnlinePaymentPayboxPlugin/lib/PayboxPayment.class.php
+++ plugins/liOnlinePaymentPayboxPlugin/lib/PayboxPayment.class.php
@@ -21,18 +21,18 @@
 ***********************************************************************************/
 ?>
 <?php
-  
+
   class PayboxPayment extends OnlinePayment
   {
     const name = 'paybox';
     protected $url = array();
-    protected $site, $rang, $id, $hash;
-    
+    protected $site, $rang, $id, $hash, $disable3ds;
+
     public static function create(Transaction $transaction)
     {
       return new self($transaction);
     }
-    
+
     public static function getTransactionIdByResponse(sfWebRequest $request)
     {
       return $request->getParameter('transaction_id', false);
@@ -40,7 +40,7 @@
     public function response(sfWebRequest $request)
     {
       $this->createBankPayment($request)->save();
-      
+
       // renewing the paybox's key cache
       if ( !isset($this->pem['local'] ) ) $this->pem['local']  = 'paybox.pem';
       if ( !isset($this->pem['remote']) ) $this->pem['remote'] = 'http://www1.paybox.com/telechargements/pubkey.pem';
@@ -53,11 +53,11 @@
         fwrite($fp, file_get_contents($this->pem['remote']));
       fclose($fp);
       chmod($path.$this->pem['local'], 0777);
-      
+
       // getting the paybox's public key
       $cert = file_get_contents($path.$this->pem['local']);
       $pubkeyid = openssl_get_publickey($cert);
-      
+
       $signature = base64_decode($request->getParameter('signature'));
       $get = $request->getGetParameters();
       unset($get['signature']);
@@ -65,7 +65,7 @@
       foreach ( $get as $key => $val )
         $str[] = $key.'='.$val;
       $str = implode('&',$str);
-      
+
       switch ( openssl_verify($str, $signature, $pubkeyid) ) {
       case 1:
         break;
@@ -74,10 +74,10 @@
       default:
         throw new liOnlineSaleException(sprintf('Impossible to parse this signature : %s',$signature));
       }
-      
+
       return array('success' => $get['error'] === '00000', 'amount' => $get['amount']/100);
     }
-    
+
     protected function __construct(Transaction $transaction)
     {
       // the configuration
@@ -92,30 +92,31 @@
       $this->autosubmit = sfConfig::get('app_payment_autosubmit',true);
       $this->datetime = date('c');
       $this->pem      = sfConfig::get('app_payment_pem',array());
-      
+      $this->disable3ds = sfConfig::get('app_payment_disable3ds', false);
+
       // the transaction and the amount
       parent::__construct($transaction);
     }
-    
+
     public function render(array $attributes = array())
     {
       $url = $this->getTPEWebURL();
       if ( !$url )
         return '<div class="'.$attributes['class'].'" id="'.$attributes['id'].'">Pas de serveur Paybox disponible...</div>';
-      
+
       $r = '';
       $r .= '<form action="'.$url.'" method="post" ';
       $attributes = $attributes + array('target' => '_top');
       foreach ( $attributes as $key => $value )
         $r .= $key.'="'.$value.'" ';
       $r .= '>';
-      
+
       foreach ( $this->getPayboxVars() as $name => $value )
         $r .= "\n".'<input type="hidden" name="'.$name.'" value="'.$value.'" />';
-      
+
       $r .= '<input type="image" alt="P" value="Paybox" src="https://preprod-tpeweb.paybox.com/favicon.ico" /><a href="#" onclick="javascript: console.error(this); $(this).closest(\'form\').submit(); return false;">aybox</a>';
       $r .= '</form>';
-      
+
       return $r;
     }
 
@@ -132,14 +133,14 @@
         return 'An error occurred creating the link with the bank';
       }
     }
-    
+
     public function getHmac()
     {
       $arr = array();
       foreach ( $this->getPayboxVarsWithoutHmac() as $key => $val )
         $arr[] = $key.'='.$val;
       $str = implode('&',$arr);
-      
+
       return strtoupper(hash_hmac($this->hash, $str, pack('H*',$this->key)));
     }
     public function getPayboxVars()
@@ -148,11 +149,11 @@
       $arr['PBX_HMAC'] = $this->getHmac();
       return $arr;
     }
-    
+
     public function getPayboxVarsWithoutHmac()
     {
       sfContext::getInstance()->getConfiguration()->loadHelpers('Url');
-      
+
       $arr = array();
       $arr['PBX_SITE'] = $this->site;
       $arr['PBX_RANG'] = $this->rank;
@@ -168,48 +169,50 @@
       $arr['PBX_ANNULE']   = url_for($this->url['cancel'],true);
       $arr['PBX_REFUSE']   = url_for($this->url['cancel'],true);
       $arr['PBX_REPONDRE_A'] = url_for($this->url['automatic'],true);
-      
+      if ( $this->disable3ds )
+        $arr['PBX_3DS'] = 1;
+
       return $arr;
     }
-    
+
     // get a functionnal web server for bank requests
     public function getTPEWebURL()
     {
       $r = false;
-      
+
       if ( !isset($this->url['payment']) )
         throw new liOnlineSaleException('The Paybox module is not configured properly, there is no payment url.');
-      
+
       foreach ( $this->url['payment'] as $url )
       {
         if ( count($this->url['payment']) == 1 )
           return $this->url['payment'][0].$this->url['uri'];
-        
+
         $doc = new DOMDocument();
         $doc->loadHTMLFile($url.'load.html');
-        
+
         $status = '';
         $element = $doc->getElementById('server_status');
         if ( $element )
           $status = $element->textContent;
-        
+
         if ( $status == 'OK' )
         {
           $r = $url.$this->url['uri'];
           break;
         }
       }
-      
+
       return $r;
     }
-    
+
     public function createBankPayment(sfWebRequest $request)
     {
       $bank = new BankPayment;
-      
+
       if (! $request instanceof sfWebRequest )
         throw new liOnlineSaleException('We cannot save the raw data from the bank.');
-      
+
       $bank->code = $request->getParameter('error');
       $bank->payment_certificate = $request->getParameter('signature');
       $bank->authorization_id = $request->getParameter('paybox_id');
@@ -219,10 +222,10 @@
       $bank->transaction_id = $request->getParameter('transaction_id');
       $bank->amount = $request->getParameter('amount');
       $bank->raw = $_SERVER['QUERY_STRING'];
-      
+
       return $this->BankPayment = $bank;
     }
-    
+
     public function getProviderTransactionId()
     {
       return $this->BankPayment instanceof BankPayment ? $this->BankPayment->authorization_id : false;
