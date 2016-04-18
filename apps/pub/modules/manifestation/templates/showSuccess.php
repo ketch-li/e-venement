be8da51d2d0031c42d8dd0c5f2bd8dbeea89b341
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-04-18 11:03:08 +0200

    (pub) allow translations of app_texts_synthetic_plans and app_texts_synthetic_categories - refs #1114

--- apps/pub/modules/manifestation/templates/showSuccess.php
+++ apps/pub/modules/manifestation/templates/showSuccess.php
@@ -10,32 +10,26 @@
   <?php use_stylesheet('pub-manifestation-synthetic?'.date('Ymd')) ?>
   <?php use_javascript('pub-manifestation-synthetic?'.date('Ymd')) ?>
   <?php use_javascript('pub-seated-plan?'.date('Ymd')) ?>
-  
+
   <?php include_partial('show_synthetic_full', array('gauges' => $gauges)) ?>
-  
+
   <div id="tickets">
     <?php include_partial('show_named_tickets', array('manifestation' => $manifestation)) ?>
   </div>
   <div id="container">
-    <?php
-      $texts = pubConfiguration::getText('app_texts_synthetic', array());
-      foreach ( array('plans', 'categories') as $field )
-      if ( !isset($texts[$field]) )
-        $texts[$field] = '';
-    ?>
     <h4 data-tab="#plans"><?php echo __('Choice by the seating') ?></h4>
     <h4 class="hidden" data-tab="#categories"><?php echo __('Automatic choice by category') ?></h4>
     <div class="tab" id="plans">
       <div class="li-content">
         <?php include_partial('show_plans', array('manifestation' => $manifestation)) ?>
-        <div class="description"><?php echo $texts['plans'] ?></div>
+        <div class="description"><?php pubConfiguration::getText('app_texts_synthetic_plans', ''); ?></div>
         <div class="clear"></div>
       </div>
     </div>
     <div class="tab hidden" id="categories">
       <div class="li-content">
         <?php include_partial('show_categories', array('manifestation' => $manifestation)) ?>
-        <div class="description"><?php echo $texts['categories'] ?></div>
+        <div class="description"><?php echo pubConfiguration::getText('app_texts_synthetic_categories', ''); ?></div>
         <div class="clear"></div>
       </div>
     </div>
@@ -45,13 +39,13 @@
     <?php echo nl2br(pubConfiguration::getText('app_texts_manifestation_bottom')) ?>
   </div>
   <?php include_partial('global/show_links', array('objects' => $manifestation)) ?>
-  
+
 </div>
 <?php else: ?>
-  
+
   <?php include_partial('show_gauges', array('gauges' => $gauges, 'manifestation' => $manifestation, 'form' => $form, 'mcp' => $mcp, )) ?>
   <?php include_partial('show_footer', array('manifestation' => $manifestation)) ?>
   <?php include_partial('show_ease') ?>
   <?php include_partial('global/show_links', array('objects' => $manifestation)) ?>
-  
+
 <?php endif ?>
