47c2770eaecda45be66d20584082ee87bb083699
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2016-07-01 15:41:40 +0200

    (tck) correcting commit a4f4f8e about printing out barcodes & QRCodes refs #1579

--- apps/tck/modules/ticket/templates/_ticket_html.php
+++ apps/tck/modules/ticket/templates/_ticket_html.php
@@ -47,10 +47,10 @@
     <p class="ticket-bc"><?php
     switch ( sfConfig::get('app_tickets_id') ) {
     case 'barcode':
-      //echo '<img src="'.url_for('ticket/barcode?id='.$ticket->id).'" />';
+      echo '<img src="'.url_for('ticket/barcode?id='.$ticket->id).'" />';
       break;
     default:
-      //echo image_tag('/liBarcodePlugin/php-barcode/barcode.php?scale=1&code='.$ticket->getIdBarcoded());
+      echo image_tag('/liBarcodePlugin/php-barcode/barcode.php?scale=1&code='.$ticket->getIdBarcoded());
       break;
     }
     ?></p>
