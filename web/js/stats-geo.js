b90fd30b279cc2c064e5602096184ba9bffda88d
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2016-07-19 17:09:31 +0200

    (stats) Geographical approach graphs from flash to jqplot refs #1569

--- web/js/stats-geo.js
+++ web/js/stats-geo.js
@@ -9,14 +9,13 @@ $(document).ready(function(){
 });
 
 LI.stats.geo = function(){
+  var approach = $('#criterias_approach').children('option[selected="selected"]').val();
 
   $('#content .jqplot').each(function(){
-console.log($(this));
+
     var chart = $(this).find('.chart')
     var name = chart.attr('data-series-name');
-console.log(name);
     var id = chart.prop('id');
-console.log(id);
     var title = $(this).find('h2').prop('title') ? $(this).find('h2').prop('title')+': ' : '';
     LI.csvData[name] = [
       [
@@ -25,29 +24,36 @@ console.log(id);
       ],
     ]; 
     
+    //retrieve stats
     $.get(chart.attr('data-json-url') + '?type=' + name, function(json){
       var array = [];
       var series = [];
-      console.log(json);
-      switch ( name ) {
-      case 'ego':
-        $.each(json.tickets, function(key, value) {
-          console.log(key + '-' + value);
-          array.push([json.translations[key], value]);
-          LI.csvData[name].push([json.translations[key], value]);
-        });
-        break;
-      default:
-       $.each(json.value, function(key, value) {
-          array.push([json.translations[key], value]);
-          LI.csvData[name].push([json.translations[key], value]);
-        });
+      //build data array depending on approach filter
+      switch ( approach ) {
+        case 'by-tickets':
+          $.each(json.tickets, function(key, value) {
+            array.push([json.translations[key], value]);
+            LI.csvData[name].push([json.translations[key], value]);
+          });
+          break;
+        case 'financial':
+          $.each(json.value, function(key, value) {
+            array.push([json.translations[key], value]);
+            LI.csvData[name].push([json.translations[key], value]);
+          });
+          break;
+        default:
+         $.each(json.nb, function(key, value) {
+            array.push([json.translations[key], value]);
+            LI.csvData[name].push([json.translations[key], value]);
+          });
       }
       
+      //init jqplot with data array
       $.jqplot(id, [array], {
         seriesDefaults: {
           rendererOptions: {
-            fill: false,
+            fill: true,
             showDataLabels: true,
             slideMargin: 4,
             lineWidth: 5
