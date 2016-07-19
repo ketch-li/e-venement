2bd9670821a4d4010584cf6878032d72f24b5e9d
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2016-07-19 12:21:42 +0200

    (event) activating the filter on prices' description, since it has migrated to a translation table refs #1671

--- lib/filter/doctrine/PriceFormFilter.class.php
+++ lib/filter/doctrine/PriceFormFilter.class.php
@@ -44,4 +44,11 @@ class PriceFormFilter extends BasePriceFormFilter
     $q->andWhere('pt.name ILIKE ?', $value['text'].'%');
     return $q;
   }
+  public function addDescriptionColumnQuery(Doctrine_Query $q, $field, $value)
+  {
+    if (!( $value && is_array($value) && isset($value['text']) && $value['text'] ))
+      return $q;
+    $q->andWhere('pt.description ILIKE ?', $value['text'].'%');
+    return $q;
+  }
 }
