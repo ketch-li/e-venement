e5c219a1da977f82124d4f173b1f85eb9e3021a1
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2016-07-20 11:43:55 +0200

    (stats) Debts stats from flash to jqplot refs #1569

--- apps/stats/modules/debts/templates/indexSuccess.php
+++ apps/stats/modules/debts/templates/indexSuccess.php
@@ -1,11 +1,10 @@
-<?php //include_partial('attendance/filters',array('form' => $form)) ?>
-<div class="ui-widget ui-corner-all ui-widget-content">
-  <div class="ui-widget-header ui-corner-all fg-toolbar">
-    <?php //include_partial('attendance/filters_buttons') ?>
-    <h1><?php echo __('Debts evolution',array(),'menu') ?></h1>
-  </div>
-  <div class="chart">
-    <?php echo liWidgetOfc::createChart(900, 530, $sf_context->getModuleName().'/data',true); ?>
-  </div>
-  <div class="actions"><?php include_partial('global/chart_actions') ?></div>
-</div>
+
+<?php include_partial('global/chart_jqplot', array(
+        'id'    => 'debts',
+        'data'  => cross_app_url_for('stats', 'debts/json'),
+        'label' => __('Debts evolution',null,'menu'),
+        'width' => '900'
+       )) ?>
+<?php use_javascript('/js/jqplot/plugins/jqplot.dateAxisRenderer.js') ?>
+<?php use_javascript('/js/jqplot/plugins/jqplot.cursor.js') ?>
+<?php use_javascript('stats-debts?'.date('Ymd')) ?>
