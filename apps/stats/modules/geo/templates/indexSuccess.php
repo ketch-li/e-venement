59bfd63b4910c3ed6c0ac0f1a776fe67ed8a52c8
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2016-07-20 14:45:37 +0200

    (stats) Added user access rights check refs #1569

--- apps/stats/modules/geo/templates/indexSuccess.php
+++ apps/stats/modules/geo/templates/indexSuccess.php
@@ -9,70 +9,72 @@
   </div>
 <?php include_partial('show_criterias') ?>
 <?php include_partial('show_header') ?>
-<?php include_partial('global/chart_jqplot', array(
-        'id'    => 'ego',
-        'data'  => cross_app_url_for('stats', 'geo/json'),
-        'label' => __('From your localization'),
-        'width' => '100%',
-        'class' => 'geo charts-4'
-       )) 
-?>
-<?php $client = sfConfig::get('app_about_client', array()) ?>
-
-<?php if ( isset($client['postalcode']) && is_array($client['postalcode']) ): ?>
+<?php if ( $sf_user->hasCredential('stats-geo') ): ?>
   <?php include_partial('global/chart_jqplot', array(
-          'id'    => 'metropolis-in',
+          'id'    => 'ego',
           'data'  => cross_app_url_for('stats', 'geo/json'),
-          'label' => __('Your metropolis'),
+          'label' => __('From your localization'),
           'width' => '100%',
           'class' => 'geo charts-4'
          )) 
   ?>
-<?php endif ?>
-<?php if ( Doctrine::getTable('GeoFrStreetBase')->createQuery('sb')->count() > 0 ): ?>
+  <?php $client = sfConfig::get('app_about_client', array()) ?>
+
+  <?php if ( isset($client['postalcode']) && is_array($client['postalcode']) ): ?>
+    <?php include_partial('global/chart_jqplot', array(
+            'id'    => 'metropolis-in',
+            'data'  => cross_app_url_for('stats', 'geo/json'),
+            'label' => __('Your metropolis'),
+            'width' => '100%',
+            'class' => 'geo charts-4'
+           )) 
+    ?>
+  <?php endif ?>
+  <?php if ( Doctrine::getTable('GeoFrStreetBase')->createQuery('sb')->count() > 0 ): ?>
+    <?php include_partial('global/chart_jqplot', array(
+            'id'    => 'districts',
+            'data'  => cross_app_url_for('stats', 'geo/json'),
+            'label' => __('By district'),
+            'width' => '100%',
+            'class' => 'geo charts-4'        
+           )) 
+    ?>
+  <?php endif ?>
   <?php include_partial('global/chart_jqplot', array(
-          'id'    => 'districts',
+          'id'    => 'postalcodes',
           'data'  => cross_app_url_for('stats', 'geo/json'),
-          'label' => __('By district'),
+          'label' => __('By postalcode'),
           'width' => '100%',
-          'class' => 'geo charts-4'        
+          'class' => 'geo charts-4'       
          )) 
   ?>
+  <?php include_partial('global/chart_jqplot', array(
+          'id'    => 'departments',
+          'data'  => cross_app_url_for('stats', 'geo/json'),
+          'label' => __('By department'),
+          'width' => '100%',
+          'class' => 'geo charts-4'
+         )) 
+  ?>
+  <?php include_partial('global/chart_jqplot', array(
+          'id'    => 'regions',
+          'data'  => cross_app_url_for('stats', 'geo/json'),
+          'label' => __('By region'),
+          'width' => '100%',
+          'class' => 'geo charts-4'
+         ))
+  ?>
+  <?php include_partial('global/chart_jqplot', array(
+          'id'    => 'countries',
+          'data'  => cross_app_url_for('stats', 'geo/json'),
+          'label' => __('By country'),
+          'width' => '100%',
+          'class' => 'geo charts-4'
+          )
+        ) 
+  ?>
+  <div class="clear"></div>
 <?php endif ?>
-<?php include_partial('global/chart_jqplot', array(
-        'id'    => 'postalcodes',
-        'data'  => cross_app_url_for('stats', 'geo/json'),
-        'label' => __('By postalcode'),
-        'width' => '100%',
-        'class' => 'geo charts-4'       
-       )) 
-?>
-<?php include_partial('global/chart_jqplot', array(
-        'id'    => 'departments',
-        'data'  => cross_app_url_for('stats', 'geo/json'),
-        'label' => __('By department'),
-        'width' => '100%',
-        'class' => 'geo charts-4'
-       )) 
-?>
-<?php include_partial('global/chart_jqplot', array(
-        'id'    => 'regions',
-        'data'  => cross_app_url_for('stats', 'geo/json'),
-        'label' => __('By region'),
-        'width' => '100%',
-        'class' => 'geo charts-4'
-       ))
-?>
-<?php include_partial('global/chart_jqplot', array(
-        'id'    => 'countries',
-        'data'  => cross_app_url_for('stats', 'geo/json'),
-        'label' => __('By country'),
-        'width' => '100%',
-        'class' => 'geo charts-4'
-        )
-      ) 
-?>
-<div class="clear"></div>
 </div>
 <?php use_javascript('/js/jqplot/plugins/jqplot.pieRenderer.js') ?>
 <?php use_javascript('/js/jqplot/plugins/jqplot.cursor.js') ?>
