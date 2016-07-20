fa9cbbb02c8674bde265cb9607b3fe041ba6c484
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2016-07-20 14:47:01 +0200

    (stats) Removed label so that it is not displayed twice refs #1569

--- apps/stats/modules/debts/templates/indexSuccess.php
+++ apps/stats/modules/debts/templates/indexSuccess.php
@@ -1,10 +1,18 @@
-
-<?php include_partial('global/chart_jqplot', array(
-        'id'    => 'debts',
-        'data'  => cross_app_url_for('stats', 'debts/json'),
-        'label' => __('Debts evolution',null,'menu'),
-        'width' => '900'
-       )) ?>
+<div class="ui-widget ui-corner-all ui-widget-content">
+  <div class="ui-widget-header ui-corner-all fg-toolbar">
+    <h1><?php echo __('Debts evolution',array(),'menu') ?></h1>
+  </div>
+  <?php use_helper('CrossAppLink') ?>
+  <?php if ( $sf_user->hasCredential('stats-activity') ): ?>
+	  <?php include_partial('global/chart_jqplot', array(
+	          'id'    => 'debts',
+	          'data'  => cross_app_url_for('stats', 'debts/json'),
+	          'label' => '',
+	          'width' => '900'
+	         )) ?>
+  <?php endif ?>
+  </div>
+</div>
 <?php use_javascript('/js/jqplot/plugins/jqplot.dateAxisRenderer.js') ?>
-<?php use_javascript('/js/jqplot/plugins/jqplot.cursor.js') ?>
-<?php use_javascript('stats-debts?'.date('Ymd')) ?>
+  <?php use_javascript('/js/jqplot/plugins/jqplot.cursor.js') ?>
+  <?php use_javascript('stats-debts?'.date('Ymd')) ?>
