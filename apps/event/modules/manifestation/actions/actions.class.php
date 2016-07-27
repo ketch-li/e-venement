839250418d39decedb9a296902b5a311a2be08db
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2016-07-27 12:44:09 +0200

    (event) adding a feature that allows to toggle the online state of gauges in the list of manifestations refs #1382

--- apps/event/modules/manifestation/actions/actions.class.php
+++ apps/event/modules/manifestation/actions/actions.class.php
@@ -459,6 +459,16 @@ class manifestationActions extends autoManifestationActions
     $this->getUser()->setFlash('success', __('The provided manifestations have been applied to "%%event%%".', array('%%event%%' => $event)));
     $this->redirect('manifestation/index');
   }
+  public function executeBatchToggleOnlineGauges(sfWebRequest $request)
+  {
+    $q = Doctrine::getTable('Gauge')->createQuery('g')
+      ->andWhereIn('g.manifestation_id', $request->getParameter('ids'));
+    foreach ( $q->execute() as $gauge )
+    {
+      $gauge->online = !$gauge->online;
+      $gauge->save();
+    }
+  }
   public function executeBatchPeriodicity(sfWebRequest $request)
   {
     $arg = 'periodicity[manifestation_id][%%i%%]=';
