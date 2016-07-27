27151a09c1923ce26e4bc68bdcb7add80bbc9319
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2016-07-27 15:40:29 +0200

    (stats) Social stats from flash to jqplot refs #1569

--- apps/stats/modules/social/templates/indexSuccess.php
+++ apps/stats/modules/social/templates/indexSuccess.php
@@ -1,3 +1,4 @@
+<?php use_helper('CrossAppLink') ?>
 <?php include_partial('attendance/filters',array('form' => $form)) ?>
 <div class="ui-widget ui-corner-all ui-widget-content">
   <a name="chart-title"></a>
@@ -5,8 +6,34 @@
     <?php include_partial('attendance/filters_buttons') ?>
     <h1><?php echo __('Social statistics',null,'menu') ?></h1>
   </div>
+  <?php include_partial('global/chart_jqplot', array(
+            'id'    => 'fs',
+            'data'  => cross_app_url_for('stats', 'social/json'),
+            'width' => '100%',
+            'class' => 'charts-4'
+          )) 
+    ?>
+  <?php include_partial('global/chart_jqplot', array(
+            'id'    => 'fq',
+            'data'  => cross_app_url_for('stats', 'social/json'),
+            'width' => '100%',
+            'class' => 'charts-4'
+          )) 
+    ?>
+  <?php include_partial('global/chart_jqplot', array(
+            'id'    => 'tor',
+            'data'  => cross_app_url_for('stats', 'social/json'),
+            'width' => '100%',
+            'class' => 'charts-4'
+          )) 
+    ?>
 <?php include_partial('chart_fs') ?>
 <?php include_partial('chart_fq') ?>
 <?php include_partial('chart_tor') ?>
-<div class="clear"></div>
+	<div class="clear"></div>
 </div>
+
+<?php use_javascript('/js/jqplot/plugins/jqplot.pieRenderer.js') ?>
+<?php use_javascript('/js/jqplot/plugins/jqplot.cursor.js') ?>
+<?php use_javascript('stats-social?'.date('Ymd')) ?>
+
