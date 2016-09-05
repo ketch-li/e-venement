da9ad22c6cf9e7d0176ddbfc9930acd0ce768b6a
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2016-09-05 17:13:32 +0200

    (default) Added mechanism to delete failed login attempts after a successful one and refactored actions refs #1839

--- apps/default/modules/sfGuardAuth/actions/actions.class.php
+++ apps/default/modules/sfGuardAuth/actions/actions.class.php
@@ -37,77 +37,34 @@ class sfGuardAuthActions extends BasesfGuardAuthActions
     $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfGuardFormSignin'); 
     $this->form = new $class();
     $userName = $request->getParameter('signin')['username'];
-
-    switch($this->isBanned($request))
-    {
-      case 'ban':
-
-        sfContext::getInstance()->getUser()->setFlash('error',sfConfig::get('project_anti_brute_force_ban_msg', __('Please contact your administrator')));
-        $this->form->getErrorSchema()->addError(new sfValidatorError(new sfValidatorSchema(), __('The username and/or password is invalid.')), 'username');
-        $this->form->setDefault('username', $userName);
-        break;
-      case 'last':
-
-        $this->signIn($request);
-        sfContext::getInstance()->getUser()->setFlash('notice',sfConfig::get('project_anti_brute_force_last_attempt_msg', __('Last attempt !')));
-        break;
-      default:
-
-        $this->signIn($request);
-    }
-  }
-  public function executeError404(sfWebRequest $request)
-  {
-  }
-
-  protected function isBanned($request)
-  {
-    $userName = $request->getParameter('signin')['username'];
     $ip = $request->getHttpHeader('addr','remote');
 
+    $timeThreshold  = sfConfig::get('project_anti_brute_force_time_threshold', '30 minutes');
     $loginThreshold = sfConfig::get('project_anti_brute_force_login_threshold', 5);
     $ipThreshold    = sfConfig::get('project_anti_brute_force_ip_threshold', 20);
-    $timeThreshold  = sfConfig::get('project_anti_brute_force_time_threshold', 30);
-
-    $lastAttempt = false;
-    $banned = false;
-
-    $sameLoginAttempts = Doctrine::getTable('Authentication')->createQuery('a')
-      ->where('a.description = ?', $userName)
-      ->andWhere('a.created_at > (NOW() - ?::INTERVAL)', $timeThreshold)
-      ->andWhere('a.success = false')
-      ->fetchArray()
-    ;
     
-    $sameIpAttempts = Doctrine::getTable('Authentication')->createQuery('a')
-      ->where('a.ip_address = ?', $ip)
-      ->andWhere('a.created_at > (NOW() - ?::INTERVAL)', $timeThreshold)
-      ->andWhere('a.success = false')
-      ->fetchArray()
-    ;
+    $attempts = $this->getLoginAttempts($userName, $ip, $timeThreshold);
 
-    if( count($sameLoginAttempts) > $loginThreshold || count($sameIpAttempts) > $ipThreshold )
+    // Login or IP is already banned
+    if ( $attempts['login'] >= $loginThreshold || $attempts['ip'] >= $ipThreshold )
     {
-      return 'ban';
-    }else if( count($sameLoginAttempts) == ($loginThreshold - 1) || count($sameIpAttempts) == ($ipThreshold - 1) )
-    {
-      return 'last';
+      $user->setFlash('error',sfConfig::get('project_anti_brute_force_ban_msg', __('Please contact your administrator')),false);
+      $this->form->getErrorSchema()->addError(new sfValidatorError(new sfValidatorSchema(), __('The username and/or password is invalid.')), 'username');
+      $this->form->setDefault('username', $userName);
+      $this->dispatcher->notify(new sfEvent($this, 'user.auth_failed'));
     }
-    return 'ok';
-  }
 
-  protected function signIn($request)
-  {
-    $user = $this->getUser();
-
-    if ($request->isMethod('post'))
+    // log in attempt 
+    if ( $request->isMethod('post') )
     {
       $this->form->bind($request->getParameter('signin'));
 
-      if ($this->form->isValid())
+      if ( $this->form->isValid() )
       {
         $values = $this->form->getValues(); 
         $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
+
+        $this->resetCount($userName, $ip, $timeThreshold);
 
         // always redirect to a URL set in app.yml
         // or to the referer
@@ -115,29 +72,75 @@ class sfGuardAuthActions extends BasesfGuardAuthActions
         $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url', $user->getReferer($request->getReferer()));
 
         return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
-        }
-        $this->dispatcher->notify(new sfEvent($this, 'user.auth_failed'));
       }
-      else
+
+      // an error occured
+      $this->dispatcher->notify(new sfEvent($this, 'user.auth_failed'));
+      $attempts['ip']++;
+      $attempts['login']++;
+      // preparing the display of a new login form
+      if ( $attempts['login'] == $loginThreshold || $attempts['ip'] == $ipThreshold ) // is now banned
+        $user->setFlash('error',sfConfig::get('project_anti_brute_force_ban_msg', __('Please contact your administrator')),false);
+      if ( $attempts['login'] == $loginThreshold - 1 || $attempts['ip'] == $ipThreshold - 1 ) // last attempt
+        $user->setFlash('notice',sfConfig::get('project_anti_brute_force_last_attempt_msg', __('Last attempt !')),false); 
+    }
+    else
+    {
+      if ( $request->isXmlHttpRequest() )
       {
-        if ( $request->isXmlHttpRequest() )
-        {
-          $this->getResponse()->setHeaderOnly(true);
-          $this->getResponse()->setStatusCode(401);
-
-          return sfView::NONE;
-        }
-        // if we have been forwarded, then the referer is the current URL
-        // if not, this is the referer of the current request
-        $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
-
-        $module = sfConfig::get('sf_login_module');
-        if ( $this->getModuleName() != $module )
-        {
-          return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
-        }
-
+        $this->getResponse()->setHeaderOnly(true);
         $this->getResponse()->setStatusCode(401);
-        }
+
+        return sfView::NONE;
+      }
+      // if we have been forwarded, then the referer is the current URL
+      // if not, this is the referer of the current request
+      $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
+
+      $module = sfConfig::get('sf_login_module');
+      if ( $this->getModuleName() != $module )
+      {
+        return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
+      }
+
+      $this->getResponse()->setStatusCode(401);
+    }
+  }
+
+  public function executeError404(sfWebRequest $request)
+  {
+  }
+
+  protected function getLoginAttempts($userName, $ip, $timeThreshold)
+  {
+
+    $sameLoginAttempts = Doctrine::getTable('Authentication')->createQuery('a')
+      ->where('a.description = ?', $userName)
+      ->andWhere('a.created_at > (NOW() - ?::INTERVAL)', $timeThreshold)
+      ->andWhere('a.success = false')
+      ->count()
+    ;
+    
+    $sameIpAttempts = Doctrine::getTable('Authentication')->createQuery('a')
+      ->where('a.ip_address = ?', $ip)
+      ->andWhere('a.description != ?', '__Logout__')
+      ->andWhere('a.created_at > (NOW() - ?::INTERVAL)', $timeThreshold)
+      ->andWhere('a.success = false')
+      ->count()
+    ;
+
+    return array('login' => $sameLoginAttempts, 'ip' => $sameIpAttempts);
+  }
+
+  protected function resetCount($userName, $ip, $timeThreshold)
+  {
+    Doctrine_Query::create()
+      ->delete()
+      ->from('Authentication a')
+      ->where('a.ip_address = ?', $ip)
+      ->andWhere('a.description = ?', $userName)
+      ->andWhere('a.created_at > (NOW() - ?::INTERVAL)', $timeThreshold)
+      ->andWhere('a.success = false')
+      ->execute(); 
   }
 }
