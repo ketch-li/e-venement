6c73574e6e0fba75926e25921e024b916bbac292
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2016-09-20 10:57:02 +0200

    (event) Added back to list button to event templating

--- apps/event/modules/manifestation/templates/templatingSuccess.php
+++ apps/event/modules/manifestation/templates/templatingSuccess.php
@@ -4,9 +4,14 @@
   <div class="fg-toolbar ui-widget-header ui-corner-all action">
     <h1><?php echo __('Templating') ?></h1>
   </div>
-  
   <?php include_partial('manifestation/flashes') ?>
-  
+  <p class="back">
+    <a class="fg-button ui-state-default fg-button-icon-left" href="<?php echo url_for('@event') ?>">
+      <span class="ui-icon ui-icon-arrowreturnthick-1-w"></span>
+      <?php echo __('Back to list',null,'sf_admin') ?>
+    </a>
+  </p>
+  <div class="ui-widget-content ui-corner-all">
   <?php echo $form->renderFormTag(url_for('manifestation/templating')) ?>
     <?php echo $form->renderHiddenFields(); ?>
     <table>
@@ -14,4 +19,5 @@
       <tr><td></td><td><input type="submit" name="submit" value="<?php echo __('Apply') ?>" /></td></tr>
     </table>
   </form>
+  </div>
 </div>
