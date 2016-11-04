a3c2b82f1df0f30fdd581c8cd93ff31272ac8eb4
Baptiste SIMON <baptiste.simon@libre-informatique.fr>
2016-11-04 15:48:55 +0100

    (pub) correcting a bad condition on selecting the private stylesheet of the application refs #2272

--- apps/pub/modules/layout/templates/_stylesheets.php
+++ apps/pub/modules/layout/templates/_stylesheets.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" media="screen" href="/css/<?php echo $public_stylesheet ?>" />
-<?php if ( $sf_context->getConfiguration()->getApplication() == 'public' ): ?>
+<?php if ( $sf_context->getConfiguration()->getApplication() == 'pub' ): ?>
 <link rel="stylesheet" type="text/css" media="screen" href="/private/public.css?<?php echo date('Ymd') ?>" />
 <?php else: ?>
 <link rel="stylesheet" type="text/css" media="screen" href="/private/<?php echo $sf_context->getConfiguration()->getApplication() ?>.css?<?php echo date('Ymd') ?>" />
