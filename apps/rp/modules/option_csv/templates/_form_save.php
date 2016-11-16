34beaf03a6bbbd281adda906930eaab5b9f3ec76
Fabrice <fabrice.petrequin@libre-informatique.fr>
2016-11-16 10:21:44 +0100

    (rp) adding a correct label on a checkbox of the extraction options refs #2465

--- apps/rp/modules/option_csv/templates/_form_save.php
+++ apps/rp/modules/option_csv/templates/_form_save.php
@@ -1,5 +1,19 @@
+<fieldset>
+
+    <div class="line ui-corner-all sf_admin_form_row sf_admin_field_<?php echo $form['select-all']->getName() ?> <?php echo $form['select-all']->hasError() ? 'ui-state-error' : '' ?>">
+      <?php echo $form['select-all']->renderLabel() ?>
+      <?php echo $form['select-all'] ?>
+      <?php if ($form['select-all']->hasError()): ?>
+      <div class="errors">
+        <span class="ui-icon ui-icon-alert floatleft"></span>
+        <?php echo $form['select-all']->renderError() ?>
+      </div>
+      <?php endif; ?>
+    </div>
+
+</fieldset>
+
   <fieldset class="sf_admin_action_save">
-    <?php echo $form['select-all'] ?>
     <button type="submit" class="fg-button ui-state-default fg-button-icon-left"><span class="ui-icon ui-icon-circle-check"></span>Mettre à jour</button>
   </fieldset>
   <div style="clear: both"></div>
