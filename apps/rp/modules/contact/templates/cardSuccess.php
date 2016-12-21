b55d2e359fe4734af1a5997c40781611707414d6
Bevlhay FERRAND <bevlhay.ferrand@libre-informatique.fr>
2016-12-21 14:13:16 +0100

    (rp) Adding informations and modify structuration of member_card creation

--- apps/rp/modules/contact/templates/cardSuccess.php
+++ apps/rp/modules/contact/templates/cardSuccess.php
@@ -31,28 +31,60 @@ $(document).ready(function(){
 <div class="member_card">
   <div class="content card">
     <p class="picture"></p>
+    <p class="cardid"><span class="title"><?php echo __('N° mumber card') ?></span><?php echo(' '.$card->id); ?></p>
     <p class="name"><span class="title"><?php echo __('Name') ?></span> <?php echo $contact->name ?></p>
     <p class="firstname"><span class="title"><?php echo __('Firstname') ?></span> <?php echo $contact->firstname ?></p>
-    <p class="barcode"><?php echo image_tag('/liBarcodePlugin/php-barcode/barcode.php?scale=1&code='.$contact->getIdBarcoded()); ?></p>
+    <p class="address"><span class="title"><?php echo __('Address') ?></span><?php echo nl2br(trim($contact->address)) ?></p>
+    <p class="city"><?php echo $contact->postalcode.' '.$contact->city ?></p>
+    <p class="country"><?php echo $contact->country ?></p>
+    <p class="email"><span class="title"><?php echo __('Email') ?></span><?php echo $contact->email ?></p>
+    <p class="date"><span class="title"><?php echo __('Expiration') ?></span> <?php echo format_date($card->expire_at) ?></p>
+    <p class="barcode">
+        <?php
+            switch(sfConfig::get('app_cards_id', 'id') )
+            {
+                case 'qrcode':
+                    echo("<img src='data:image/png;base64,".$card->QRcodeBase64PNG."' alt='QRcode #".$card->id."' />");
+                break;
+                case 'id':
+                    echo image_tag('/liBarcodePlugin/php-barcode/barcode.php?scale=1&code='.$contact->getIdBarcoded());
+                break;
+            }
+         ?>
+    </p>
     <p class="extra-card"><?php echo nl2br(sfConfig::get('app_cards_extra')) ?></p>
   </div>
   <div class="content archive">
-    <p class="name"><span class="title"><?php echo __('Name') ?></span><br/><?php echo $contact->name ?></p>
-    <p class="firstname"><span class="title"><?php echo __('Firstname') ?></span><br/><?php echo $contact->firstname ?></p>
-    <p class="address"><span class="title"><?php echo __('Address') ?></span><br/><?php echo nl2br(trim($contact->address)) ?></p>
+    <p class="cardid"><span class="title"><?php echo __('N° mumber card') ?></span><?php echo(' '.$card->id); ?></p>
+    <p class="name"><span class="title"><?php echo __('Name') ?></span><?php echo $contact->name ?></p>
+    <p class="firstname"><span class="title"><?php echo __('Firstname') ?></span><?php echo $contact->firstname ?></p>
+    <p class="address"><span class="title"><?php echo __('Address') ?></span><?php echo nl2br(trim($contact->address)) ?></p>
     <p class="city"><?php echo $contact->postalcode.' '.$contact->city ?></p>
     <p class="country"><?php echo $contact->country ?></p>
+    <p class="email"><span class="title"><?php echo __('Email') ?></span><?php echo $contact->email ?></p>
     <p class="status"><span class="title"><?php echo __('Status') ?></span> <?php echo nl2br(__($card->name)) ?></p>
     <p class="date"><span class="title"><?php echo __('Expiration') ?></span> <?php echo format_date($card->expire_at) ?></p>
-    <p class="barcode"><?php echo image_tag('/liBarcodePlugin/php-barcode/barcode.php?scale=1&code='.$contact->getIdBarcoded()); ?></p>
+    <p class="barcode">
+        <?php
+            switch(sfConfig::get('app_cards_id', 'id') )
+            {
+                case 'qrcode':
+                    echo("<img src='data:image/png;base64,".$card->QRcodeBase64PNG."' alt='QRcode #".$card->id."' />");
+                break;
+                case 'id':
+                    echo image_tag('/liBarcodePlugin/php-barcode/barcode.php?scale=1&code='.$contact->getIdBarcoded());
+                break;
+            }
+         ?>
+    </p>
   </div>
   <div class="content receipt">
-    <p class="cardid"><span class="title">N°</span><?php echo(' '.$card->id); ?></p>
     <p class="librinfo">Imprimé et géré par e-venement www.libre-informatique.fr</p>
+    <p class="cardid"><span class="title"><?php echo __('N° mumber card') ?></span><?php echo(' '.$card->id); ?></p>
     <h2><?php echo __('Card receipt') ?></h2>
-    <p class="name"><span class="title"><?php echo __('Name') ?></span><br/><?php echo $contact->name ?></p>
-    <p class="firstname"><span class="title"><?php echo __('Firstname') ?></span><br/><?php echo $contact->firstname ?></p>
-    <p class="address"><span class="title"><?php echo __('Address') ?></span><br/><?php echo nl2br(trim($contact->address)) ?></p>
+    <p class="name"><span class="title"><?php echo __('Name') ?></span><?php echo $contact->name ?></p>
+    <p class="firstname"><span class="title"><?php echo __('Firstname') ?></span><?php echo $contact->firstname ?></p>
+    <p class="address"><span class="title"><?php echo __('Address') ?></span><?php echo nl2br(trim($contact->address)) ?></p>
     <p class="city"><?php echo $contact->postalcode.' '.$contact->city ?></p>
     <p class="country"><?php echo $contact->country ?></p>
     <p class="status"><span class="title"><?php echo __('Status') ?></span> <?php echo nl2br(__($card->name)) ?></p>
