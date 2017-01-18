d5f48410e8986c6d41b29b4e86883fa69a2af677
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2017-01-18 11:41:49 +0100

    (bin) adding a script to specifically update submodules

--- bin/migration-to-v211.sh
+++ bin/migration-to-v211.sh
@@ -63,13 +63,7 @@ do let "i++";  [ $i -eq 3 ] && NBTR=$elt; done
 
 read -p "Do you want to pull all your git submodules ? [Y/n] " subm
 if [ "$subm" != "n" ]; then
-  git submodule init
-  git submodule update
-  for elt in lib/vendor/externals/*; do
-    (cd $elt; git checkout -f origin/master; git pull origin master && git checkout master && git pull)
-  done
-  for elt in `find lib/vendor/externals/ -type d`; do chmod -R a+rx $elt; done
-  echo "If you had permissions errors previously, it probably means that you are not the file owner. Please execute 'sudo for elt in `find lib/vendor/externals/ -type d`; do chmod -R a+rx $elt; done'"
+  ./bin/update-submodules.sh
 fi
 
 echo ""
