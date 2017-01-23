c66e934e9a4608662a491e822c921661fde35d6b
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2017-01-23 15:04:04 +0100

    (pos) correcting a graphical bug refs #2770

--- apps/pos/modules/product/templates/_list_category.php
+++ apps/pos/modules/product/templates/_list_category.php
@@ -1,3 +1,5 @@
+<?php if ( $product->product_category_id ): ?>
 <span style="background-color: <?php echo $product->Category->Color ?>;">
   <?php echo $sf_user->hasCredential('pos-product-category') ? link_to($product->Category, 'category/edit?id='.$product->product_category_id) : $product->Category ?>
 </span>
+<?php endif ?>
