232758e7427e617012c6bd2ddd15c4ef0b0a387d
Baptiste SIMON (Libre Informatique) <beta@e-glop.net>
2017-01-26 17:19:32 +0100

    (pub) optimizing the display of seated plans refs #3006

--- apps/pub/modules/manifestation/templates/showSuccess.php
+++ apps/pub/modules/manifestation/templates/showSuccess.php
@@ -25,17 +25,23 @@
 
   <?php include_partial('show_synthetic_full', array('gauges' => $gauges)) ?>
 
+  <?php
+    $seated = new Doctrine_Collection('Gauge');
+    $unseated = new Doctrine_Collection('Gauge');
+    foreach ( $gauges as $gauge )
+    if (!( $gauge->Workspace->seated && $gauge->getSeatedPlan() ))
+      $unseated[] = $gauge;
+    else
+      $seated[] = $gauge;
+  ?>
   <div id="tickets">
     <?php
-      $unseated = new Doctrine_Collection('Gauge');
-      foreach ( $gauges as $gauge )
-      if ( !$gauge->getSeatedPlan() )
-        $unseated[] = $gauge;
       if ( $unseated->count() > 0 )
         include_partial('show_gauges', array('gauges' => $unseated, 'manifestation' => $manifestation, 'form' => $form, 'mcp' => $mcp, 'show_name' => true, ));
     ?>
     <?php include_partial('show_named_tickets', array('manifestation' => $manifestation)) ?>
   </div>
+  <?php if ( $seated->count() > 0 ): ?>
   <div id="container">
     <h4 data-tab="#plans"><?php echo __('Choice by the seating') ?></h4>
     <h4 class="hidden" data-tab="#categories"><?php echo __('Automatic choice by category') ?></h4>
@@ -54,6 +60,7 @@
       </div>
     </div>
   </div>
+  <?php endif ?>
   <div class="clear"></div>
   <div class="text_config manifestation_bottom synthetic_plans">
     <?php echo nl2br(pubConfiguration::getText('app_texts_manifestation_bottom')) ?>
