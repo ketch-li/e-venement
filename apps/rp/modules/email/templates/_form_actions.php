13284688cc9acb39b572efad474a9c49761bb66c
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2017-02-21 09:57:49 +0100

    (rp) - Added a save button in the emailing page - refs #3156 (#142)
    * (rp) - Added a save button in the emailing page - refs #3156

--- apps/rp/modules/email/templates/_form_actions.php
+++ apps/rp/modules/email/templates/_form_actions.php
@@ -1,3 +1,4 @@
-<ul class="sf_admin_actions_form no-float">
-  <?php echo $helper->linkToList(array(  'params' => 'class= fg-button ui-state-default fg-button-icon-left ',  'class_suffix' => 'list',  'label' => 'Back to list',  'ui-icon' => '',)) ?>
+<ul class="sf_admin_actions_form">
+  <?php echo $helper->linkToList(array(  'params' => 'class= fg-button ui-state-default fg-button-icon-left ',  'class_suffix' => 'list',  'label' => 'Back to list',  'ui-icon' => '')) ?>
+  <?php echo $helper->linkToSave($form->getObject(), array(  'params' => 'class= fg-button ui-state-default fg-button-icon-left ',  'class_suffix' => 'save',  'label' => 'Save',  'ui-icon' => '')) ?>
 </ul>
