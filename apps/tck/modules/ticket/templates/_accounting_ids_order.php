a1084edf8ffb013578e3351ef2d10dc5d36b7cc4
Fabrice <fabrice.petrequin@libre-informatique.fr>
2017-03-09 18:15:34 +0100

    (tck) - Added the possibility to configure the title of order page in app.yml - refs #3277

--- apps/tck/modules/ticket/templates/_accounting_ids_order.php
+++ apps/tck/modules/ticket/templates/_accounting_ids_order.php
@@ -1,3 +1,4 @@
 <p id="ids" class="order">
-  <?php echo __('<span class="name inline-modifiable">Order</span> #<span class="order_id">%%oid%%</span>, for transaction #<span class="transaction_id">%%tid%%</span>',array('%%oid%%' => $order->id, '%%tid%%' => $transaction->id), 'li_accounting') ?>
+  <span class="name inline-modifiable"><?php echo sfConfig::get('app_transaction_order_name', __('Order', null, 'li_accounting')) ?> </span>
+  <?php echo __('#<span class="order_id">%%oid%%</span>, for transaction #<span class="transaction_id">%%tid%%</span>', array('%%oid%%' => $order->id, '%%tid%%' => $transaction->id), 'li_accounting') ?>
 </p>
--- apps/tck/modules/ticket/templates/_accounting_type_order.php
+++ apps/tck/modules/ticket/templates/_accounting_type_order.php
@@ -1,1 +1,1 @@
-<div id="type" class="order"><span class="inline-modifiable"><?php echo __('Order', null, 'li_accounting') ?></span></div>
+<div id="type" class="order"><span class="inline-modifiable"><?php echo sfConfig::get('app_transaction_order_name', __('Order', null, 'li_accounting')) ?></span></div>
