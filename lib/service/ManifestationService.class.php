a63e4b0af5be485982dd06caceda87cc17c386cf
Baptiste SIMON <baptiste.simon@e-glop.net>
2017-06-23 17:21:27 +0200

    (event) adding an option that permits to get every manifestations not regarding to user credentials

--- lib/service/ManifestationService.class.php
+++ lib/service/ManifestationService.class.php
@@ -12,7 +12,7 @@
  */
 class ManifestationsService extends EvenementService
 {
-    public function buildQuery(sfGuardUser $user, $transaction_id = 0, $alias = 'm')
+    public function buildQuery(sfGuardUser $user, $transaction_id = 0, $alias = 'm', $strict = true)
     {
         // init
         $transaction_id = $transaction_id ? $transaction_id : 0;
@@ -28,15 +28,17 @@ class ManifestationsService extends EvenementService
           ->leftJoin('g.PriceGauges pg')
           ->leftJoin('pg.Price pgp WITH pgp.hide = FALSE')
           ->leftJoin('g.Workspace w')
-          ->leftJoin('w.Order wuo ON wuo.workspace_id = w.id AND wuo.sf_guard_user_id = '.$user->getId())
           ->orderBy("et.name, met.name, $alias.happens_at, $alias.duration, wuo.rank, w.name, $alias.id")
+          ->leftJoin('w.Order wuo ON wuo.workspace_id = w.id AND wuo.sf_guard_user_id = '.$user->getId())
           ->leftJoin('pmp.WorkspacePrices pmpwp WITH pmpwp.workspace_id = w.id')
           ->leftJoin('pmp.UserPrices      pmpup WITH pmpup.sf_guard_user_id = '.$user->getId())
           ->leftJoin('pgp.WorkspacePrices pgpwp WITH pgpwp.workspace_id = w.id')
           ->leftJoin('pgp.UserPrices      pgpup WITH pgpup.sf_guard_user_id = '.$user->getId())
           ->leftJoin('w.WorkspaceUsers wsu WITH wsu.sf_guard_user_id = '.$user->getId())
-          ->andWhere('wsu.sf_guard_user_id IS NOT NULL')
         ;
+        if ( $strict ) {
+          $q->andWhere('wsu.sf_guard_user_id IS NOT NULL');
+        }
         
         return $q;
     }
