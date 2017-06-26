212cd221a3f8a332ca8d055a6b0d6060c91b15b6
Baptiste SIMON <baptiste.simon@e-glop.net>
2017-06-26 17:15:53 +0200

    (global) add a service for MemberCards features

--- apps/pub/lib/myUser.class.php
+++ apps/pub/lib/myUser.class.php
@@ -463,65 +463,7 @@ class myUser extends pubUser
   
   public function getAvailableMCPrices($manifestation = NULL)
   {
-    $mcp = array();
-    try {
-
-    $mcs = $this->getTransaction()->contact_id
-      ? $this->getContact()->MemberCards
-      : $this->getTransaction()->MemberCards;
-    if ( $mcs->count() == 0 )
-      return $mcp;
-    
-    // get back available prices
-    foreach ( $mcs as $mc )
-    if ( $mc->active || $mc->transaction_id == $this->getTransactionId() )
-    foreach ( $mc->MemberCardPrices as $price )
-    {
-      $event_id = is_null($price->event_id) ? '' : $price->event_id;
-      
-      if ( $event_id && $manifestation instanceof Manifestation && $price->event_id != $manifestation->event_id )
-        continue;
-      
-      if ( !isset($mcp[$price->price_id][$event_id]) )
-        $mcp[$price->price_id][$event_id] = 0;
-      
-      $mcp[$price->price_id][$event_id]++;
-    }
-    
-    // get back already booked tickets
-    $q = Doctrine_Query::create()->from('Ticket tck')
-      ->select('tck.*, m.event_id AS event_id')
-      ->andWhere('tck.printed_at IS NULL')
-      ->andWhere('tck.member_card_id IS NOT NULL OR t.id = ?', $this->getTransactionId())
-      ->leftJoin('tck.Manifestation m')
-      ->leftJoin('m.Event e')
-      ->leftJoin('e.Manifestations em')
-      ->leftJoin('tck.Price p')
-      ->andWhere('p.member_card_linked = ?',true)
-      ->leftJoin('tck.Transaction t')
-      ->andWhere('t.contact_id = ?',$this->getContact()->id)
-      ->leftJoin('t.Order o')
-    ;
-    if ( $manifestation )
-      $q->andWhere('em.id = ?', $manifestation->id)
-        ->andWhere('o.id IS NOT NULL OR t.id = ? AND tck.manifestation_id != em.id', $this->getTransactionId())
-      ;
-    else
-      $q->andWhere('o.id IS NOT NULL OR t.id = ?', $this->getTransactionId());
-      
-    $tickets_to_count = $q->execute();
-    foreach ( $tickets_to_count as $ticket )
-    {
-      if ( isset($mcp[$ticket->price_id][$ticket->event_id]) )
-        $mcp[$ticket->price_id][$ticket->event_id]--;
-      elseif ( isset($mpc[$ticket->price_id]['']) )
-        $mcp[$ticket->price_id]['']--;
-    }
-    
-    return $mcp;
-    
-    }
-    catch ( liEvenementException $e )
-    { return $mcp; }
+    return sfContext::getInstance()->getContainer()->get('member_cards_service')
+        ->getAvailableMCPrices($this->getTransaction(), $manifestation);
   }
 }
