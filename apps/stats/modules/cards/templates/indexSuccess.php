96d87690de44179d6576247e115e8a9a95e2eb78
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2017-08-30 09:36:55 +0200

    (stats) - Change the request for membercards creation - refs #4296 (#379)

--- apps/stats/modules/cards/templates/indexSuccess.php
+++ apps/stats/modules/cards/templates/indexSuccess.php
@@ -3,7 +3,7 @@
 <?php include_partial('filters',array('form' => $form)) ?>
 <div class="ui-widget ui-corner-all ui-widget-content cards">
   <div class="ui-widget-header ui-corner-all fg-toolbar">
-    <h1><?php echo __('Memberships-like from %%from%% to %%to%%',array('%%from%%' => format_date($dates['from']), '%%to%%' => format_date($dates['to']))) ?></h1>
+    <h1><?php echo __('Membercard creation from %%from%% to %%to%%',array('%%from%%' => format_date($dates['from']), '%%to%%' => format_date($dates['to']))) ?></h1>
     <?php include_partial('attendance/filters_buttons') ?>
   </div>
   <?php include_partial('global/chart_help'); ?>
@@ -15,21 +15,7 @@
             'name'  => __('Member cards')
           )) 
     ?>
-  <div class="ui-widget-content ui-corner-all accounting">
-  <form action="" method="get">
-    <p><span><?php echo __('VAT:') ?></span><span><input type="text" name="accounting[vat]" value="<?php echo isset($accounting['vat']) ? $accounting['vat'] : 0 ?>" />%</span></p>
-    <?php foreach ( $cards as $card ): ?>
-    <p>
-      <span><?php echo __('Prices for %%price%%',array('%%price%%' => __($card['name']))) ?>:</span>
-      <span><input type="text" name="accounting[price][<?php echo $card['name'] ?>]" value="<?php echo isset($accounting['price'][$card['name']]) ? $accounting['price'][$card['name']] : 0 ?>" /><?php echo $sf_context->getConfiguration()->getCurrency() ?></span>
-    </p>
-    <?php endforeach ?>
-    <p><span></span><span><input type="submit" name="submit" value="ok" /></span></p>
-  </form>
-  </div>
-  <p class="ui-widget-content ui-corner-all warning">
-    <?php echo __('This chart is calculated on the full selected period. If a member card expires or has been created within it, the total quantity will be impacted with a fraction of this member card and not a full one.') ?>
-  </p>
+  
 <?php endif ?>
 </div>
 
