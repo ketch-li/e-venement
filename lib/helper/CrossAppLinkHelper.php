355764e2707acc95feb1903ae7855b3ea7957ee1
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2017-10-03 16:15:13 +0200

    Revert because it needs more testing - refs #3921 (#443)
    * Revert "(lib) - Fix a bug when creating a url with # fragment - refs #4711 (#442)"
    
    This reverts commit bab8e71601305c3472c0056fce3d1bea6bb18621.
    
    * Revert "(lib) - Fix a bug on container creation with sfDependencyInjectionPlugin - refs #3921 (#436)"
    
    This reverts commit b743f28f89bcad4719355c50a91acdf5184125fc.

--- lib/helper/CrossAppLinkHelper.php
+++ lib/helper/CrossAppLinkHelper.php
@@ -5,18 +5,6 @@
  * @author Libre Informatique [http://www.libre-informatique.fr/]
  */
 
- function getAppRouting($route_file)
- {
-   $frontendRouting = new sfPatternRouting(new sfEventDispatcher());
-
-   $config = new sfRoutingConfigHandler();
-   $routes = $config->evaluate(array(sfConfig::get('sf_apps_dir') . DIRECTORY_SEPARATOR . $route_file . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'routing.yml'));
-
-   $frontendRouting->setRoutes($routes);
-
-   return $frontendRouting;
- }
-
 /**
  * return an url for a given symfony application and an internal url
  *
@@ -31,68 +19,50 @@
  * @param boolean $debug
  * @return string
  */
-function cross_app_url_for($appname, $parameters, $absolute = false, $env = null, $debug = NULL)
+function cross_app_url_for($appname, $url, $absolute = false, $env = null, $debug = NULL)
 {
-
-    $route = '';
-    $fragment = '';
-
-    if (is_string($parameters))
-    {
-      // absolute URL or symfony URL?
-      if (preg_match('#^[a-z][a-z0-9\+.\-]*\://#i', $parameters))
-      {
-        return $parameters;
-      }
-
-      // relative URL?
-      if (0 === strpos($parameters, '/'))
-      {
-        return $parameters;
-      }
-
-      if ($parameters == '#')
-      {
-        return $parameters;
-      }
-
-      // strip fragment
-      if (false !== ($pos = strpos($parameters, '#')))
-      {
-        $fragment = substr($parameters, $pos + 1);
-        $parameters = substr($parameters, 0, $pos);
-      }
-
-      list($route, $params) = sfContext::getInstance()->getController()->convertUrlStringToParameters($parameters);
-    }
-    else if (is_array($parameters))
-    {
-      if (isset($parameters['sf_route']))
-      {
-        $route = $parameters['sf_route'];
-        unset($parameters['sf_route']);
-      }
-    }
-
-
-  $url = getAppRouting($appname)->generate($route, $params, $absolute);
+  $initial_app = sfContext::getInstance()->getConfiguration()->getApplication();
+  $initial_web_controler = basename(sfContext::getInstance()->getRequest()->getScriptName());
+  $initial_config = sfConfig::getAll();
+  $stack = sfContext::getInstance()->getActionStack();
   
   // get the environment
   if ( $env == null )
   {
     $env = sfContext::getInstance()->getConfiguration()->getEnvironment();
   }
-
-  $initial_app = sfContext::getInstance()->getConfiguration()->getApplication();
-  $initial_web_controler = basename(sfContext::getInstance()->getRequest()->getScriptName());
-
-
+  
+  // get the debug context
+  if ( $debug === NULL )
+  {
+    $debug = sfConfig::get('sf_debug', false);
+  }
+  
+  // context creation
+  if (!sfContext::hasInstance($appname))
+  {
+    $context = sfContext::createInstance(ProjectConfiguration::getApplicationConfiguration($appname, $env, $debug), $appname);
+    $context->getEventdispatcher()->notify(new sfEvent($context, 'context.load_factories'));
+  }
+  else
+  {
+    $context = sfContext::getInstance($appname);
+  }
   $web_url = str_replace(
     $initial_web_controler.'/'.$initial_web_controler,
     $initial_web_controler,
-    $url
+    $context->getController()->genUrl($url, $absolute)
   );
-  
+  sfContext::switchTo($initial_app);
+  sfConfig::add($initial_config);
+  unset($context);
+  if ( sfContext::getInstance()->getActionStack()->getSize() != $stack->getSize() )
+  {
+    while ( sfContext::getInstance()->getActionStack()->popEntry() );
+    while ( $entry = $stack->popEntry() )
+      sfContext::getInstance()->getActionStack()->addEntry($entry->getModuleName(), $entry->getActionName(), $entry);
+  }
+
   //remove initial web controler
   // genUrl use $this->context->getRequest()->getScriptName();, its a call to $_SERVER
   // so starting the shameless part !
@@ -106,19 +76,12 @@ function cross_app_url_for($appname, $parameters, $absolute = false, $env = null
     $script_name="index";
   }
   $script_name.='.php';
-
+  
   // check if this file exist
   if (!file_exists(sfConfig::get('sf_web_dir').DIRECTORY_SEPARATOR.$script_name))
     throw new sfException('can t find '.$script_name.' in the web directory');
   
   $web_url = str_replace ($initial_web_controler, $script_name, $web_url);
-
-  $web_url = '/' . $script_name . $web_url;
-
-  if ($fragment)
-  {
-    $web_url .= '#'.$fragment;
-  }
 
   return $web_url;
 }
