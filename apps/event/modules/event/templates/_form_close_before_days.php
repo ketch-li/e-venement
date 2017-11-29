139a19c31dc1b060d4593899e84dd31fb3c1132d
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2017-11-29 11:24:16 +0100

    (event) - Change the behavior and display of close_before field - refs #5134 (#521)

--- apps/event/modules/event/templates/_form_close_before_days.php
+++ apps/event/modules/event/templates/_form_close_before_days.php
@@ -21,21 +21,36 @@
 *
 ***********************************************************************************/
 ?>
-<div class="sf_admin_form_row sf_admin_text sf_admin_form_field_close_before_days">
-<label for="close_before_days"><?php echo __('days') ?></label>
-<div class="label ui-helper-clearfix"></div>
-<input type="number" name="close_before_days" value="0" min="0" />
-<script type="text/javascript"><!--
-$(document).ready(function(){
-  $('.sf_admin_form_field_close_before input').change(function(){
-    var hrs_close = parseInt($(this).val().replace(/:\d\d$/, ''))/24;
-    $('.sf_admin_form_field_close_before_days input').val(Math.floor(hrs_close));
-  }).change();
-  $('.sf_admin_form_field_close_before_days input').change(function(){
-    var hrs_close = parseInt($('.sf_admin_form_field_close_before input').val().replace(/:\d\d$/, ''))%24 + $(this).val()*24;
-    var mins_close = $('.sf_admin_form_field_close_before input').val().replace(/^-{0,1}\d+:/, '');
-    $('.sf_admin_form_field_close_before input').val(hrs_close+':'+mins_close);
-  });
-});
---></script>
+<?php $event = $form->getObject(); ?>
+<div>
+  <div class="sf_admin_form_row sf_admin_text sf_admin_form_field_close_before">
+    <label for="event_close_before"><?php echo __('Online sales close before'); ?></label>
+    <div class="label ui-helper-clearfix"></div>
+    <div class="widget ">
+      <input type="text" name="event[close_before]" value="<?php echo $event->close_before; ?>" id="event_close_before" />
+    </div>
+  </div>
+
+  <div class="sf_admin_form_row sf_admin_text sf_admin_form_field_close_before_days">
+    <label for="close_before_days"><?php echo __('days') ?></label>
+    <div class="label ui-helper-clearfix"></div>
+    <input type="number" name="close_before_days" value="0" min="0" />
+    <script type="text/javascript"><!--
+    
+      $(document).ready(function() {
+        $('#event_close_before').change(function() {
+          var hrs_close = parseInt($(this).val().replace(/:\d\d$/, ''))/24;
+          $('.sf_admin_form_field_close_before_days input').val(Math.floor(hrs_close));
+        });
+        
+        $('.sf_admin_form_field_close_before_days input').change(function(){
+          var hrs_close = parseInt($('#event_close_before').val().replace(/:\d\d$/, ''))%24 + $(this).val()*24;
+          var mins_close = $('#event_close_before').val().replace(/^-{0,1}\d+:/, '');
+          $('#event_close_before').val(hrs_close+':'+mins_close);
+        });
+        
+      });
+  --></script>
+  </div>
+
 </div>
