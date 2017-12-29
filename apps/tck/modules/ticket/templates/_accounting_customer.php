99cf18acaf4f3005868a6af64a385e2b6720a31e
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2017-12-29 09:24:42 +0100

    (tck) - Switch organism name and contact name - refs #5214 (#538)

--- apps/tck/modules/ticket/templates/_accounting_customer.php
+++ apps/tck/modules/ticket/templates/_accounting_customer.php
@@ -1,11 +1,11 @@
 <div id="customer">
+  <p class="orgname inline-modifiable"><?php echo $transaction->Professional->Organism->name ?></p>
   <p class="name inline-modifiable"><?php echo $transaction->Contact->getNameWithTitle() ?></p>
   <p class="phonenumber inline-modifiable"><?php
     echo is_null($transaction->professional_id)
     ? ($transaction->Contact->Phonenumbers->count() > 0 ? $transaction->Contact->Phonenumbers[0]->number : '')
     : ($transaction->Professional->contact_number ? $transaction->Professional->contact_number : ($transaction->Professional->Organism->Phonenumbers->count() > 0 ? $transaction->Professional->Organism->Phonenumbers[0]->number : ''))
   ?></p>
-  <p class="orgname inline-modifiable"><?php echo $transaction->Professional->Organism->name ?></p>
   <p class="address inline-modifiable"><?php echo nl2br(ucwords(strtolower($transaction->professional_id ? $transaction->Professional->Organism->address : $transaction->Contact->address))) ?></p>
   <p class="postalcode inline-modifiable"><?php echo $transaction->professional_id ? $transaction->Professional->Organism->postalcode : $transaction->Contact->postalcode ?></p>
   <p class="city inline-modifiable"><?php echo strtoupper($transaction->professional_id ? $transaction->Professional->Organism->city : $transaction->Contact->city) ?></p>
