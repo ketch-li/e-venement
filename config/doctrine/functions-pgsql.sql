58a0d069ac0b3d4e6322a3cede56d344297bdce4
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2018-01-31 09:39:46 +0100

    (config) - Change how pgsql functions are created - refs #5534 (#560)

--- config/doctrine/functions-pgsql.sql
+++ config/doctrine/functions-pgsql.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION sum_aggreg (p integer, n boolean) RETURNS integer AS 
   SELECT $2::integer + $1 AS result;
 $$ LANGUAGE sql IMMUTABLE;
 
-DROP AGGREGATE sum(boolean);
+DROP AGGREGATE IF EXISTS sum(boolean);
 
 CREATE AGGREGATE sum (
   basetype = boolean,
